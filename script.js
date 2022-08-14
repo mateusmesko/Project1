be75e65dd21b433febc91af186051407766158e8
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2022-08-13 22:40:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,20 +7,10 @@ fetch("db.json")
     data.posts.forEach(function (dados){
        
         console.log(dados.comments)
-        document.body.innerHTML+=`<h1>começo</h1><audio id="player" src="./Audio1.mp3"></audio>
-        <div class="player">
-          <div class="control">
-              <i class="fas fa-play" id="playbtn"></i>
-          </div>
-      <div class="info">
-              TheFatRat - Electrified
-          <div class="bar">
-              <div id="progress"></div>
-          </div>
-      </div>
-    
-      <div id="current">0:00</div>
-      </div>`;
+        document.body.innerHTML+=`<h1>começo</h1>
+        <audio controls id="player" >
+            <source src="./Audio1.mp3">
+        </audio>`;
 
         dados.comments.forEach(function(dados){
             console.log(dados)
@@ -48,34 +38,3 @@ var playpause = function () {
   }
 }
 
-playbtn.addEventListener("click", playpause);
-
-player.onplay = function () {
-  playbtn.classList.remove("fa-play");
-  playbtn.classList.add("fa-pause");
-}
-
-player.onpause = function () {
-  playbtn.classList.add("fa-play");
-  playbtn.classList.remove("fa-pause");
-}
-
-player.ontimeupdate = function () {
-  let ct = player.currentTime;
-  current.innerHTML = timeFormat(ct);
-  //progress
-  let duration = player.duration;
-  prog = Math.floor((ct * 100) / duration);
-  progress.style.setProperty("--progress", prog + "%");
-}
-
-function timeFormat(ct) {
-  minutes = Math.floor(ct / 60);
-  seconds = Math.floor(ct % 60);
-
-  if (seconds < 10) {
-    seconds = "0"+seconds;
-  }
-
-  return minutes + ":" + seconds;
-}
